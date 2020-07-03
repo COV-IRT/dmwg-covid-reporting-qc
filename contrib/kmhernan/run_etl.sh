037eacc13bdb8c21a56332cf6493425e749393a6
Kyle Hernandez <kmhernan84@gmail.com>
2020-07-03 13:34:04 -0500

    feat(etl): add files that are ok to distribute

---
+++ contrib/kmhernan/run_etl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+DATADIR="./data"
+UF_WEEKLY_DEATHS="covid_deaths_usafacts.weekly.csv"
+UF_WEEKLY_CASES="covid_cases_usafacts.weekly.csv"
+
+function print_usage() {
+    echo "Usage: run_etl.sh <command>"
+    echo "Commands:"
+    echo "    usafacts - extract weekly death/cases from USAFacts"
+}
+
+function run_usafacts() {
+    echo "Running Rscript..."
+    Rscript ./etls/usafacts.county.etl.R \
+    --out_weekly_deaths ${DATADIR}/${UF_WEEKLY_DEATHS} \
+    --out_weekly_cases ${DATADIR}/${UF_WEEKLY_CASES}
+
+    # compressing
+    echo "Compressing outputs..." 
+    gzip ${DATADIR}/${UF_WEEKLY_DEATHS}
+    gzip ${DATADIR}/${UF_WEEKLY_CASES}
+}
+
+if [[ -z $1 ]]; then
+    print_usage
+    exit 1
+fi
+
+if [[ $1 == "usafacts" ]]; then
+    echo "Running USA Facts ETL..."
+    run_usafacts
+else
+    echo "Unknown command $1"
+    print_usage
+    exit 1
+fi
